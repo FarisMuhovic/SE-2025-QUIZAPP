5e091f099e18561397f57f9843d5d54572a05441
FarisMuhovic <faris.fare123456@gmail.com>
2025-06-14 01:09:27 +0200

    Update constants.js

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -2,7 +2,7 @@ const constants = {
   apiURL:
     location.hostname == "127.0.0.1" || location.hostname == "localhost"
       ? "http://127.0.0.1/quiz-app/rest"
-      : "https://shark-app-x3ncs.ondigitalocean.app",
+      : "https://quizappse.netlify.app/",
   noDataBanner: `<img src="./images/emptybox.svg" alt="empty banner" class="empty-banner" />`,
   errorBanner: function (text) {
     return `<h3 class="error-banner" >${text}</h3>`
